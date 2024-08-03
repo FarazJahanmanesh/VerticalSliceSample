b73ef98333de56e4b2c8bcd130e8ea6b14171df3
farazasax@outlook.com <farazasax@outlook.com>
2024-08-03 09:47:27 +0330

    Add project files.

---
+++ Sample.Api/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sample.Api.Entities;
+namespace Sample.Api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+    public DbSet<Book> Books { get; set; }
+}
---
+++ Sample.Api/Entities/Book.cs
@@ -0,0 +1,8 @@
+﻿namespace Sample.Api.Entities;
+
+public class Book
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+}
