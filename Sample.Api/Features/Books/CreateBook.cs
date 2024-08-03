99ea9dcb81a972deca0382431ac4477ee91eb8f0
farazasax@outlook.com <farazasax@outlook.com>
2024-08-03 11:39:09 +0330

    create and read for book entitiy

---
+++ Sample.Api/Features/Books/CreateBook.cs
@@ -0,0 +1,77 @@
+﻿using Carter;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Routing;
+using Sample.Api.Data;
+using Sample.Api.Entities;
+
+namespace Sample.Api.Features.Books;
+
+//public static class CreateBook
+//{
+//    public static void AddEndPoint(this IEndpointRouteBuilder endpointRouteBuilder)
+//    {
+//        endpointRouteBuilder.MapPost("api/books", async (CreateBookCommand request, ISender sender) =>
+//        {
+//            var bookId = await sender.Send(request);
+
+//            return Results.Ok(bookId);
+//        });
+//    }
+//}
+
+public record CreateBookCommand(string Name ,string Description) :IRequest<long>;
+
+public class CreateBookValidator : AbstractValidator<CreateBookCommand>
+{
+    public CreateBookValidator()
+    {
+        RuleFor(d => d.Name)
+            .NotEmpty();
+
+        RuleFor(d => d.Description)
+            .NotEmpty();
+    }
+}
+
+internal class CreateBookHandler : IRequestHandler<CreateBookCommand, long>
+{
+    private readonly AppDbContext _dbContext;
+    private readonly IValidator<CreateBookCommand> _validator;
+    public CreateBookHandler(AppDbContext dbContext, IValidator<CreateBookCommand> validator)
+    {
+        _dbContext = dbContext;
+        _validator = validator;
+    }
+    public Task<long> Handle(CreateBookCommand request, CancellationToken cancellationToken)
+    {
+        var validationResult = _validator.Validate(request);
+        if (!validationResult.IsValid)
+        {
+            throw new BadHttpRequestException(string.Join(Environment.NewLine,validationResult.Errors.Select(d=>d.ErrorMessage)));
+        }
+
+        var book = new Book
+        {
+            Name = request.Name,
+            Description = request.Description,
+        };
+        _dbContext.Books.Add(book);
+        _dbContext.SaveChanges();
+
+        return Task.FromResult(book.Id);
+    }
+}
+
+public class CreateBookEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("api/books", async (CreateBookCommand request, ISender sender) =>
+        {
+            var bookId = await sender.Send(request);
+
+            return Results.Ok(bookId);
+        });
+    }
+}
---
+++ Sample.Api/Features/Books/GetAllBooks.cs
@@ -0,0 +1,51 @@
+﻿using Carter;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using Sample.Api.Data;
+using Sample.Api.Entities;
+
+namespace Sample.Api.Features.Books;
+
+//public static class GetAllBooks
+//{
+//    public static void AddEndPoint(this IEndpointRouteBuilder endpointRouteBuilder)
+//    {
+//        endpointRouteBuilder.MapGet("api/books",async(ISender sender) =>
+//        {
+//            var books = await sender.Send(new GetAllBooksCommand());
+
+//            return Results.Ok(books);
+//        });
+//    }
+//}
+
+public record GetAllBooksCommand:IRequest<List<Book>>;
+
+internal class GetAllBooksHandler : IRequestHandler<GetAllBooksCommand, List<Book>>
+{
+    private readonly AppDbContext _dbContext;
+    public GetAllBooksHandler(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    public Task<List<Book>> Handle(GetAllBooksCommand request, CancellationToken cancellationToken)
+    {
+        var books = _dbContext.Books.AsNoTracking().ToList();
+
+        return Task.FromResult(books);
+    }
+}
+public class GetAllBooksEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("api/books", async (ISender sender) =>
+        {
+            var books = await sender.Send(new GetAllBooksCommand());
+
+            return Results.Ok(books);
+        });
+    }
+}
